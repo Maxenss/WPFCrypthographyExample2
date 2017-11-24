7c5a64d48e11be8c57447437c550255533480622
Maxenss <kokayni4@gmail.com>
2017-11-24 02:07:24 +0200

    Initial

---
+++ WPFCrypthographyExample2/WPFCrypthographyExample2/Cryptography.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+
+namespace WPFCrypthographyExample2
+{
+    class Cryptograghy
+    {
+        private static char[] letters = {
+            'а', 'б', 'в', 'г', 'д', 'е', 'ё',
+            'ж', 'з', 'и', 'й', 'к', 'л', 'м',
+            'н', 'о', 'п', 'р','с', 'т', 'у', 'ф',
+            'х', 'ц', 'ч', 'ш', 'щ', 'ъ',
+            'ы', 'ь', 'э', 'ю', 'я'};
+
+        public static string Encode(string word, string key)
+        {
+            string resultWord = "";
+            char[] wordCharArray = word.ToCharArray();
+            int startIndex = 0;
+
+            if (!IsWordCorrect(word) || !IsWordCorrect(key))
+                return "Некорректное значение";
+
+            for (int i = 0; i < wordCharArray.Length; i++)
+            {
+                startIndex = IndexOf(wordCharArray[i]);
+                char encodedChar = GetEncodedChar(startIndex, IndexOf(key[i]));
+                wordCharArray[i] = encodedChar;
+            }
+
+            resultWord = new string(wordCharArray);
+
+            return resultWord;
+        }
+
+        public static string Decode(string word, string key)
+        {
+            string resultWord = "";
+            char[] wordCharArray = word.ToCharArray();
+            int startIndex = 0;
+
+            if (!IsWordCorrect(word) || !IsWordCorrect(key))
+                return "Некорректное значение";
+
+            for (int i = 0; i < wordCharArray.Length; i++)
+            {
+                startIndex = IndexOf(wordCharArray[i]);
+                char decodedChar = GetDecodedChar(startIndex, IndexOf(key[i]));
+                wordCharArray[i] = decodedChar;
+            }
+
+            resultWord = new string(wordCharArray);
+
+            return resultWord;
+        }
+
+        private static bool IsWordCorrect(string word)
+        {
+            char[] wordCharArray = word.ToCharArray();
+
+            for (int i = 0; i < wordCharArray.Length; i++)
+                if (!letters.Contains<char>(wordCharArray[i]))
+                    return false;
+
+            return true;
+        }
+
+        private static int IndexOf(char ch)
+        {
+            for (int i = 0; i < letters.Length; i++)
+                if (ch == letters[i])
+                    return i;
+
+            return -1;
+        }
+
+        private static char GetEncodedChar(int startIndex, int key)
+        {
+            char ch = ' ';
+
+            int charIndex = (startIndex + key) % letters.Length;
+            ch = letters[charIndex];
+
+            return ch;
+        }
+
+        private static char GetDecodedChar(int startIndex, int key)
+        {
+            char ch = ' ';
+
+            int charIndex = Math.Abs(startIndex - key + letters.Length) % letters.Length;
+            ch = letters[charIndex];
+
+            return ch;
+        }
+    }
+}
---
+++ WPFCrypthographyExample2/WPFCrypthographyExample2/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCrypthographyExample2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btDecode_Click(object sender, RoutedEventArgs e)
+        {
+            string encodedWord = tbDecodeStartWord.Text;
+            string keyWord = tbDecodeKey.Text;
+            
+
+            if (encodedWord.Equals(""))
+            {
+                MessageBox.Show("Некорректное значение слова.", "Ошибка");
+                return;
+            }
+
+            if (keyWord.Equals(""))
+            {
+                MessageBox.Show("Некорректное значение ключа.", "Ошибка");
+                return;
+            }
+
+            if (encodedWord.Length != keyWord.Length)
+            {
+                MessageBox.Show("Ключ и слово должны быть одной длины", "Ошибка");
+                return;
+            }
+
+            tbDecodeResult.Text = Cryptograghy.Decode(encodedWord.ToLower(), keyWord.ToLower());
+        }
+
+        private void btEncode_Click(object sender, RoutedEventArgs e)
+        {
+            string word = tbEncodeStartWord.Text;
+            string keyWord = tbEncodeKey.Text;
+
+
+            if (word.Equals(""))
+            {
+                MessageBox.Show("Некорректное значение слова.", "Ошибка");
+                return;
+            }
+
+            if (keyWord.Equals(""))
+            {
+                MessageBox.Show("Некорректное значение ключа.", "Ошибка");
+                return;
+            }
+
+            if (word.Length != keyWord.Length) {
+                MessageBox.Show("Ключ и слово должны быть одной длины", "Ошибка");
+                return;
+            }
+
+            tbEncodeResult.Text = Cryptograghy.Encode(word.ToLower(), keyWord.ToLower());
+        }
+    }
+}
